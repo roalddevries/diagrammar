94653a529e68989f6ed025c00da36462a90dbea7
Roald de Vries <rdv@roalddevries.nl>
2011-10-15 16:30:32 +0200

    Created an object containing diagram information

---
+++ src/com/flowductivity/Diagram.java
@@ -0,0 +1,8 @@
+package com.flowductivity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Diagram {
+	public Collection<Object> elements = new ArrayList<Object>(); // TODO
+}
--- src/com/flowductivity/DiagrammarActivity.java
+++ src/com/flowductivity/DiagrammarActivity.java
@@ -5,7 +5,10 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class DiagrammarActivity extends Activity {
@@ -13,14 +16,20 @@ public class DiagrammarActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.main);
         setContentView(new DiagrammarView(this));
     }
 
     public class DiagrammarView extends View {
     	
+    	public Diagram diagram; // TODO: this should probably be an Activity-property
+    	
         public DiagrammarView(Context context) {
             super(context);
+            diagram = new Diagram();
+            Pair<Point, Point> line1 = new Pair(new Point(0, 0), new Point(64, 64));
+            diagram.elements.add(line1);
+            Pair<Point, Point> line2 = new Pair(new Point(64, 0), new Point(0, 64));
+            diagram.elements.add(line2);
         }
 
     	public void onDraw(Canvas canvas) {
@@ -28,9 +37,10 @@ public class DiagrammarActivity extends Activity {
     		Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     		paint.setColor(Color.GRAY);
     		paint.setStyle(Paint.Style.STROKE);
-    		canvas.restore();
-    		canvas.save();
-    		canvas.drawLine(0, 0, 64, 64, paint);
+    		for (Object o : diagram.elements) {
+    			Pair<Point, Point> p = (Pair<Point, Point>) o;
+        		canvas.drawLine(p.first.x, p.first.y, p.second.x, p.second.y, paint);
+    		}
     	}
     }
 }
