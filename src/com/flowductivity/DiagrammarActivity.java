705e587523f3427f2ef5298dbeae7a2af873ef52
Roald de Vries <rdv@roalddevries.nl>
2011-10-15 15:15:07 +0200

    First line on screen

--- src/com/flowductivity/DiagrammarActivity.java
+++ src/com/flowductivity/DiagrammarActivity.java
@@ -1,13 +1,36 @@
 package com.flowductivity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 public class DiagrammarActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        // setContentView(R.layout.main);
+        setContentView(new DiagrammarView(this));
+    }
+
+    public class DiagrammarView extends View {
+    	
+        public DiagrammarView(Context context) {
+            super(context);
+        }
+
+    	public void onDraw(Canvas canvas) {
+    		canvas.drawColor(Color.WHITE);
+    		Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    		paint.setColor(Color.GRAY);
+    		paint.setStyle(Paint.Style.STROKE);
+    		canvas.restore();
+    		canvas.save();
+    		canvas.drawLine(0, 0, 64, 64, paint);
+    	}
     }
 }
