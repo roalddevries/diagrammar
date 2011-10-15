591fe1cde5f35af3e2b46a64f2ee5b4495325d08
Roald de Vries <rdv@roalddevries.nl>
2011-10-15 14:25:22 +0200

    Initial commit

---
+++ src/com/flowductivity/DiagrammarActivity.java
@@ -0,0 +1,13 @@
+package com.flowductivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DiagrammarActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
